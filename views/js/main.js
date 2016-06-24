1e028d8f634090bb47e97170464658aecee9ad29
Alex Hernandez <alexh385@gmail.com>
2016-06-24 04:35:12 -0400

    Compress pizza png
    Change the pizza png in main.js
    line 537
    
    	new file:   views/images/pizza-min.png
    	modified:   views/js/main.js

--- views/js/main.js
+++ views/js/main.js
@@ -534,7 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 30; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza-min.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
